47e90bf558aa186da6107fcd2f9f9a0340737bd2
Nick <nick_bcm@mail.ru>
2014-06-12 01:10:54 +0600

    erb to haml

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,3 +1,5 @@
+include ApplicationHelper
+=begin
 def full_title(page_title)
   base_title = "Ruby on Rails Tutorial Sample App"
   if page_title.empty?
@@ -5,4 +7,4 @@ def full_title(page_title)
   else
     "#{base_title} | #{page_title}"
   end
-end
+end=end
