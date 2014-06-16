50ba3a31a8844db9df454d4b4460ebc6b407578a
Nick <nick_bcm@mail.ru>
2014-06-16 19:21:43 +0600

    Make a basic User model (including secure passwords)

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,10 +1,9 @@
 include ApplicationHelper
-=begin
-def full_title(page_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end=end
+# def full_title(page_title)
+#   base_title = "Ruby on Rails Tutorial Sample App"
+#   if page_title.empty?
+#     base_title
+#   else
+#     "#{base_title} | #{page_title}"
+#   end
+# end
